1cf644f58f16a01bb4105f9909e1c70277b69d9b
Brian Chase <brianchasemusic@gmail.com>
2022-07-22 18:39:52 -0400

    worked out initial some psuedocode with the study group.

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+// Psuedocode
+// 
+// At some point in your journey to become a full-stack
+// web developer, you’ll likely be asked to complete a
+//  coding assessment&mdash;perhaps as part of an interview 
+//  process. A typical coding assessment includes both 
+//  multiple-choice questions and interactive coding challenges. 
+//  To help familiarize you with these tests and allow you 
+//  to use the skills covered in this module, this Challenge 
+//  invites you to build a timed coding quiz with multiple-choice 
+//  questions. This app will run in the browser and will feature 
+//  dynamically updated HTML and CSS powered by JavaScript code 
+//  that you write. It will have a clean, polished, and responsive 
+//  user interface. This week’s coursework will equip you with 
+//  all the skills you need to succeed in this assignment.
+
+// Variables
+// 
+// 
+
+// click start begins quiz and timer
+// 
+// 
+// remove h1 and add h2 "question"
+// add list item buttons for answers
+// 
+   // Choosing answers //
+// 
+// when correct answer is chosen, load next question
+      // if user answer === correct
+      // then load next question
+// wrong answer subtracts time
+      // else if question is === to other answer
+      // then timer is decremented
+// 
+// list with 4 buttons/answers for each question
+    // set text of each button to an answer for current question
+    // use <ol>
+    // make <li> elements a button
+    // potential style: correct answer turns button green onClick
+//                   incorrect answer turns button red onClick
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// either all questions answered or timer = 0 ends quiz
+//      timer ends, display message and go back to homepage
+// 
+// at game ends with time, high score and initials can be entered
+//      store remaining time in score variable
+//      place to enter initials
+//      submit button
+//          checks for valid text entry
+//          if the entry is valid
+//          then stores initials and score in local storage
+// upon submission, goto highscores page // does local storage persist if we move pages // should be a yes
+//      display top high score(s)     
+// 
+// button to go back to quiz
+//      link to opening page
+// button to clear highscores
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
